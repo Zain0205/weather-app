77ce21f6cf9cc214738a34d8eab8c4a94f4a3b8e
Fahmi Zain <fahmizain000@gmail.com>
2024-02-20 20:42:56 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
-import axios from "axios";
 import { useEffect } from "react";
+import Input from "./components/Input";
+import ContextProvider from "./components/store/Context";
+import axios from "axios";
+import LocationAndTemperature from "./components/LocationAndTemperature";
 
 function App() {
   const API_KEY = "2fae13cf42faebe935a10213f3c1516d";
-  const baseURl = `https://api.openweathermap.org/data/2.5/weather?q=Jakarta&appid=${API_KEY}`;
+  const baseURl = `https://api.openweathermap.org/data/2.5/weather?q=Setiabudi&units=metric&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=Setiabudi&units=metric&appid=${API_KEY}`
+  // const image = `http://openweathermap.org/img/wn/01d@2x.png`
 
   useEffect(() => {
-    const testFetch = async () => {
-      try {
-        const response = await axios.get(baseURl);
-        console.log(response)
-      } catch (err) {
-        console.error(err);
-      } finally {
-        console.log("Berhasil")
-      }
+    const handleTest = async () => {
+      const response = await axios.get(baseURl);
+      console.log(response.data)
     };
 
-    testFetch()
+    const handleTest2 = async () => {
+      const response = await axios.get(url)
+      console.log(response.data)
+    }
 
-  }, [baseURl]);
+    handleTest2()
+     handleTest()
+  }, [baseURl, url]);
 
   return (
-    <div>
-      <h1 className="text-3xl font-semibold text-center mt-5">Coming Soon...</h1>
-      <input type="text" />
-    </div>
+    <ContextProvider>
+      <div className="w-full h-screen bg-gradient-to-br from-cyan-700 to-blue-700">
+        <div className="w-full h-full px-5 py-6 lg:w-1/2 lg:mx-auto">
+          <Input />
+          <LocationAndTemperature />
+        </div>
+      </div>
+    </ContextProvider>
   );
 }
 
