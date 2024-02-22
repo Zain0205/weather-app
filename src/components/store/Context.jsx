8e3f51ec8f35041a71c0ac039dd61cb7a3e41fa6
Fahmi Zain <fahmizain000@gmail.com>
2024-02-22 19:11:06 +0700

    Update Context

--- src/components/store/Context.jsx
+++ src/components/store/Context.jsx
@@ -1,47 +1,53 @@
 import axios from "axios";
-import { createContext, useState, useCallback, useEffect } from "react";
+import { createContext, useState, useCallback } from "react";
 
 export const WeatherContext = createContext({
   weatherData: {},
+  weatherForecastData: {},
   getWeatherData: () => {},
   getLocation: () => {},
   location: "",
 });
 
-
 export default function ContextProvider({ children }) {
   const [data, setData] = useState({});
-  const [location, setLocation] = useState("Jakarta");
+  const [forecastData, setForecastData] = useState([])
+  const [location, setLocation] = useState("");
 
-  const API_KEY = "ecc1718928482bfa646ed03241ce28b4";
+  const API_KEY = "ecc1718928482bfa646ed03241ce28b4 ";
   const baseURl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`;
-  // const url = `https://api.openweathermap.org/data/2.5/forecast?q=Setiabudi&units=metric&appid=${API_KEY}`;
-  // const image = `http://openweathermap.org/img/wn/01d@2x.png`
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=${API_KEY}`;
 
   const handleGetLocation = (e) => {
-    let locationValue = e.target.value;
-    setLocation(locationValue);
+    setLocation(e.target.value);
   };
+
+  const handleGetForecastData = useCallback(async () => {
+    try {
+      const response = axios.get(forecastUrl)
+      response.then(ress => setForecastData(ress.data.list))
+    } catch(err){
+      console.error(err)
+    }
+  }, [forecastUrl])
 
   const handleGetData = useCallback(async () => {
     try {
       const response = await axios.get(baseURl);
-      const dataFetched = response.data
-      console.log(dataFetched)
+      const dataFetched = response.data;
+      console.log(dataFetched);
       setData(dataFetched);
-      // console.log(response.data);
+      handleGetForecastData()
       setLocation("");
+      // console.log(response.data);
     } catch (err) {
       console.error(err);
     }
-  }, [baseURl]);
-  useEffect(() => {
-
-    handleGetData()
-  }, [handleGetData]);
+  }, [baseURl, handleGetForecastData]);
 
 
   let ctxValue = {
+    weatherForecastData: forecastData,
     weatherData: data,
     getWeatherData: handleGetData,
     getLocation: handleGetLocation,
